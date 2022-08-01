f7d5ec65673216efc5f21e3816c2ffdbce293671
JoaoKeuwe <joaov2872@hotmail.com>
2022-08-01 19:41:13 -0300

    feat: :ambulance: Adicionando read, readone em car.services

--- src/services/car.services.ts
+++ src/services/car.services.ts
@@ -1,6 +1,7 @@
 import IService from '../interfaces/IService';
 import { carSchema, ICar } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
+import { ErrorTypes } from '../errors/catalog';
 
 class CarService implements IService<ICar> {
   private _car:IModel<ICar>;
@@ -16,6 +17,17 @@ class CarService implements IService<ICar> {
       throw parsed.error;
     }
     return this._car.create(obj);
+  }
+
+  public async read(): Promise<ICar[]> {
+    return this._car.read();
+  }
+
+  public async readOne(_id: string): Promise<ICar> {
+    const car = await this._car.readOne(_id);
+
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+    return car;
   }
 }
 
