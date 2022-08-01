0827363594f0784dc57279de1e5759eece214249
JoaoKeuwe <joaov2872@hotmail.com>
2022-08-01 18:48:17 -0300

    feat: :lock: Adicionando app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+import 'express-async-errors';
+import errorMiddleware from './middlewares/error';
+import carRouter from './routes/car.routes';
 
 const app = express();
-
+app.use(express.json());
+app.use(carRouter);
+app.use(errorMiddleware);
 export default app;
