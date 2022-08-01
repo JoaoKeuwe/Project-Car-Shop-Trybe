f28a3e4b6fe8bead1f5845d42beea776722cbe5d
JoaoKeuwe <joaov2872@hotmail.com>
2022-08-01 18:48:48 -0300

    feat: :lipstick: Adicionando index, carController,error,Icar

---
+++ src/controllers/car.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { ICar } from '../interfaces/ICar';
+import IService from '../interfaces/IService';
+
+export default class CarController {
+  constructor(private _service: IService<ICar>) { }
+
+  public async create(
+    req: Request 
+    & { body: ICar },
+    res: Response<ICar>,
+  ) {
+    const results = await this
+      ._service.create(req.body);
+    return res.status(201)
+      .json(results);
+  }
+}
--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,6 +1,11 @@
-import { IVehicle } from './IVehicle';
+import { z } from 'zod';
+import { vehicleSchema } from './IVehicle';
 
-export interface ICar extends IVehicle {
-  doorsQty: number,
-  seatsQty: number,
-}
+const carSchema = vehicleSchema.extend({
+  doorsQty: z.number().gte(2).lte(4),
+  seatsQty: z.number().gte(2).lte(7),
+});
+
+type ICar = z.infer<typeof carSchema>;
+
+export { carSchema, ICar };
