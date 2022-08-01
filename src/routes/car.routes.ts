4ab15ed6d4f71e44d1fd4a38ce68cd39ead95f63
JoaoKeuwe <joaov2872@hotmail.com>
2022-08-01 19:51:10 -0300

    feat: :white_check_mark: Adicionando routes get, cars.read

--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -5,14 +5,18 @@ import CarService from '../services/car.services';
 
 const route = Router();
 
-const car = new CarModel();
-const carService = new CarService(car);
+const carModel = new CarModel();
+const carService = new CarService(carModel);
 const carController = new CarController(carService);
 
 route.post(
   '/cars',
   (req, res) => carController
     .create(req, res),
+);
+route.get(
+  '/cars',
+  (req, res) => carController.read(req, res),
 );
 
 export default route;
