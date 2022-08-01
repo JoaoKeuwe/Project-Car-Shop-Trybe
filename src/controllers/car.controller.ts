e5c03e38582819101c635b2d9f1588b88e897f4c
JoaoKeuwe <joaov2872@hotmail.com>
2022-08-01 19:40:10 -0300

    test: :rocket: Adicionando car.controller

--- src/controllers/car.controller.ts
+++ src/controllers/car.controller.ts
@@ -6,13 +6,26 @@ export default class CarController {
   constructor(private _service: IService<ICar>) { }
 
   public async create(
-    req: Request 
-    & { body: ICar },
+    req: Request & { body: ICar },
     res: Response<ICar>,
   ) {
-    const results = await this
-      ._service.create(req.body);
-    return res.status(201)
-      .json(results);
+    const results = await this._service.create(req.body);
+    return res.status(201).json(results);
+  }
+
+  public async read(
+    req: Request,
+    res: Response<ICar[] | null>,
+  ) {
+    const result = await this._service.read();
+    return res.status(200).json(result);
+  }
+
+  public async readOne(
+    req: Request,
+    res: Response<ICar | null>,
+  ) {
+    const result = await this._service.readOne(req.params.id);
+    return res.status(200).json(result);
   }
 }
